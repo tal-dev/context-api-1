4964fb6c7a538bbd84999a86898275379df0fa59
tal-dev <talgat.abykeev@gmail.com>
2022-06-04 02:17:57 -0500

    small updates

--- src/App.js
+++ src/App.js
@@ -16,6 +16,18 @@ const data = [
     price: 19.99,
     image: "https://images-na.ssl-images-amazon.com/images/I/41sscWDYKXL._SX397_BO1,204,203,200_.jpg"
   },
+  { 
+    id: '3',
+    title: "Design Patterns",
+    price: 17.99,
+    image: "https://images-na.ssl-images-amazon.com/images/I/51szD9HC9pL._SX395_BO1,204,203,200_.jpg"
+  },
+  { 
+    id: '4',
+    title: "Clean Code",
+    price: 29.99,
+    image: "https://images-na.ssl-images-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg"
+  },
 ]
 
 function App() {
